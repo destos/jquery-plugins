9810cee82a1edb4bc6d314ac81113a302b8649e8
Patrick <destos@gmail.com>
2010-03-12 20:23:50 -0600

    bit change commit, learning how to merg and branch in git

--- jquery.fav/jquery.fav.js
+++ jquery.fav/jquery.fav.js
@@ -1,42 +1,63 @@
 /* --------------------------------------------------------------------
 // Author: Patrick Forringer ( destos@gmail.com )
 // File info: Fav Jquery plugin
-// Ver: 0.2
-// Last modified: never ever
+// Ver: 0.3
+// Last modified: 030510
+// Changes made in experiement branch
 //
 */
 
 (function($){
 	$.fn.fav = function(options){
 				
+		// see if we have many links
 		return $(this).each(function(){
-		
-			$link = $(this).find('a');			
 			
-			var defaults = {
-				url: $link.attr('href'),
-				name: $link.attr('title')
-			};
+			if( $(this).has('a') ){
+				$links = $(this).find('a');
+			}else if( $(this).is('a') ){
+				$links = $(this);	
+			}
 			
-			// figure out how to loop through a list of value pairs to allow seperate options for each different link like {{name:"what"},{url:"http://blah.com"}}
-			var opt = $.extend(defaults,options);
+			test_function = function(){
+				alert('test');
+			}
 			
-			$link.click(function(){
+			console.log($links);
+			// if( $links.length ){
+			// 				alert($links.length);
+			// 			}
 			
-				if ( window.sidebar ) { // Mozilla Firefox Bookmark
-					window.sidebar.addPanel( opt.name,  opt.url, '' );
-				} else if( window.external ) { // IE Favorite
-					window.external.AddFavorite(  opt.url, opt.name );
-				} else if( window.opera ){ // Opera
-					$link.attr("rel","sidebar");
-					return true;
-				} else {
-					alert("Sorry! Your browser doesn't support this function. Press ctrl+D to bookmark "+opt.name+".");
-				}
+			$links.each(function(){
 				
-				return false;
-			});
+				$link = $(this);
+				
+				// get default values.
+				var defaults = {
+					url:	$link.attr('href'),
+					name:	( $link.attr('title') ) ? $link.attr('title') : $link.text()
+				};
+				// if multiple links are selected the same custom options are applied to them, figure out how to 
+				// allow seperate options
+				var opt = $.extend(defaults,options);
+				
+				console.log( opt.url );
+				$link.click(function(){
 			
+					if ( window.sidebar ) { // Mozilla Firefox Bookmark
+						window.sidebar.addPanel( opt.name,  opt.url, '' );
+					} else if( window.external ) { // IE Favorite
+						window.external.AddFavorite(  opt.url, opt.name );
+					} else if( window.opera ){ // Opera
+						$link.attr("rel","sidebar");
+						return true;
+					} else {
+						alert("Sorry! Your browser doesn't support this function. Press ctrl+D to bookmark "+opt.name+".");
+					}
+				
+					return false;
+				});
+			});
 		});
 		
 	};
